012062b5ea1f9aab6047f27f08902fce58d024fc
amine <amine@post.com>
2025-05-20 17:57:57 +0200

    deploy

--- src/middleware.php
+++ src/middleware.php
@@ -2,12 +2,17 @@
 
 use Slim\App;
 use Tuupola\Middleware\CorsMiddleware;
+use Dotenv\Dotenv;
 
 return function (App $app) {
+    $dotenv = Dotenv::createImmutable(__DIR__. '/../');
+    $dotenv->safeLoad();
+    $API_BASE_URL = $_ENV['API_BASE_URL'];
+
     $app->addRoutingMiddleware();
 
     $app->add(new CorsMiddleware([
-        "origin" => ["http://localhost:3000"],
+        "origin" => [$API_BASE_URL],
         "methods" => ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "headers.allow" => ["Content-Type", "Authorization", "X-Requested-With"],
         "headers.expose" => [],
